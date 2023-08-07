2198bbf75a96a1397539d1c50dd0e0f2d0f0d117
ghettolifestyle <112902246+ghettolifestyle@users.noreply.github.com>
2023-08-07 09:02:43 +0200

    correctly describe the amount of serifs

--- kb.py
+++ kb.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
             "title": "kevin mai's personal blog",
             "author": "kevin mai",
             "url": "https://kmai.xyz",
-            "cool_tagline": "boomer mindset. now with more serifs",
+            "cool_tagline": "boomer mindset. now with less serifs",
             "quote_under_posts": {
                 "body": "Who's gonna carry the boats? And the logs?",
                 "source": "David Goggins"
